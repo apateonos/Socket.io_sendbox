e56e5161abdd2d8a957539fc526a285982cd5265
GyuYoung Kim <apateonos@gmail.com>
2021-04-29 12:06:46 +0900

    express ping api set

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const app = require('express');
-const httpServer = require('http').createServer(app);
-const options = {};
-const io = require('socket.io')(httpServer, options);
+const app = require('express')();
+const httpServer = require('http');
+const socketIO = require('socket.io');
 
 const PORT = 3000;
-const server = http.createServer(app).listen(PORT, () => {
-  console.log('server running...');
+const server = httpServer.createServer(app).listen(PORT, () => {
+  console.log(`${PORT} server running...`);
 });
 
-const socketCORS = { cors: { origin: '*', credentials: true }};
+app.get('/ping', (req, res) => {res.send('pong!')});
+/* 
+const socketCORS = { cors: { origin: '*:*' }};
 const io = socketIO(server, socketCORS);
 
 io.on('connection', socket => {
   console.log('connection' + socket.id);
-});
+}); */
